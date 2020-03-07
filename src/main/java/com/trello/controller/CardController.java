2f6eeb5ef00edcc15c47dce7c69feaab74206bf6
ketan <ketansomvanshi007@gmail.com>
2020-03-07 18:18:11 +0530

    adding allow cors origin *

--- src/main/java/com/trello/controller/CardController.java
+++ src/main/java/com/trello/controller/CardController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("board/{boardId}/list/{listId}")
 public class CardController {
