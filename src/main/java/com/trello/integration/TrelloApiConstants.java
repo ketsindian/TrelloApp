3aab353b42a84a8e3d01dcf8bc39c21bc9962428
ketan <ketansomvanshi007@gmail.com>
2020-03-12 21:04:45 +0530

    integrating trello api

---
+++ src/main/java/com/trello/integration/TrelloApiConstants.java
@@ -0,0 +1,13 @@
+package com.trello.integration;
+
+public class TrelloApiConstants {
+
+    public static final String TRELLO_KEY = "key=f233915c55aa2f6fd77468bfe162dc28";
+
+    public static final String TRELLO_TOKEN = "&token=ed59c7d68582fa4f9638a6957f813786b55bfa36522734fbc37d1a5ec7a11906";
+
+    public static final String BOARD_URL= "https://api.trello.com/1/boards/";
+
+    public static final String BOARD_URL_OPTIONS="/lists?cards=all&";
+    
+}
---
+++ src/main/java/com/trello/integration/TrelloCardApiResponse.java
@@ -0,0 +1,20 @@
+package com.trello.integration;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class TrelloCardApiResponse {
+    private String id;
+
+    private String name;
+
+    private List<TrelloCardApiResponse> cards;
+}
